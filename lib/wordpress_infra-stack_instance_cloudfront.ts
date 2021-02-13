98171abe256dad9f8d7eb01c58fd216f89c7a7c8
Amir Shojae <amirshojae@alphafx.co.uk>
2021-02-13 14:23:21 +0000

    changed the certificates arn and updated the ami

--- lib/wordpress_infra-stack_instance_cloudfront.ts
+++ lib/wordpress_infra-stack_instance_cloudfront.ts
@@ -6,78 +6,123 @@ import * as certificateManager from '@aws-cdk/aws-certificatemanager';
 import * as s3 from '@aws-cdk/aws-s3';
 
 export class WordpressInfraStackCloudfront extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, s3: s3.Bucket, lb: elbv2.ApplicationLoadBalancer, props?: cdk.StackProps) {
-        super(scope, id, props);
+  constructor(
+    scope: cdk.Construct,
+    id: string,
+    s3: s3.Bucket,
+    lb: elbv2.ApplicationLoadBalancer,
+    props?: cdk.StackProps
+  ) {
+    super(scope, id, props);
 
-        const myCertificate = certificateManager.Certificate.fromCertificateArn
-            (this, 'Website certificates', 'arn:aws:acm:us-east-1:460234074473:certificate/bb9f9289-08e9-4a30-ad21-bb4188db68c5');
+    const myCertificate = certificateManager.Certificate.fromCertificateArn(
+      this,
+      'Website certificates',
+      'arn:aws:acm:us-east-1:460234074473:certificate/8240e213-1d02-4742-9ade-986c9358859a'
+    );
 
+    const defaultCachePolicy = new cloudfront.CachePolicy(
+      this,
+      'Default Cache Policy',
+      {
+        cachePolicyName: 'loadBalancer',
+        comment: 'Cache policy for the load balancer (efs)',
+        headerBehavior: cloudfront.CacheHeaderBehavior.allowList(
+          'Host',
+          'Origin',
+          'Access-Control-Request-Headers',
+          'Access-Control-Request-Method',
+          'Authorization',
+          'CloudFront-Forwarded-Proto'
+        ),
+        cookieBehavior: cloudfront.CacheCookieBehavior.all(),
+        queryStringBehavior: cloudfront.CacheQueryStringBehavior.all(),
+      }
+    );
 
-        const defaultCachePolicy = new cloudfront.CachePolicy(this, 'Default Cache Policy', {
-            cachePolicyName: 'loadBalancer',
-            comment: 'Cache policy for the load balancer (efs)',
-            headerBehavior: cloudfront.CacheHeaderBehavior.allowList('Host', 'Origin', 'Access-Control-Request-Headers', 'Access-Control-Request-Method', 'Authorization', 'CloudFront-Forwarded-Proto'),
-            cookieBehavior: cloudfront.CacheCookieBehavior.all(),
-            queryStringBehavior: cloudfront.CacheQueryStringBehavior.all()
-        });
+    const s3Policy = new cloudfront.CachePolicy(this, 'S3 Cache policy', {
+      cachePolicyName: 's3',
+      headerBehavior: cloudfront.CacheHeaderBehavior.allowList(
+        'Origin',
+        'Access-Control-Request-Headers',
+        'Access-Control-Request-Method',
+        'Authorization',
+        'CloudFront-Forwarded-Proto'
+      ),
+      cookieBehavior: cloudfront.CacheCookieBehavior.none(),
+      queryStringBehavior: cloudfront.CacheQueryStringBehavior.none(),
+    });
 
-        const s3Policy = new cloudfront.CachePolicy(this, 'S3 Cache policy', {
-            cachePolicyName: 's3',
-            headerBehavior: cloudfront.CacheHeaderBehavior.allowList('Origin', 'Access-Control-Request-Headers', 'Access-Control-Request-Method', 'Authorization', 'CloudFront-Forwarded-Proto'),
-            cookieBehavior: cloudfront.CacheCookieBehavior.none(),
-            queryStringBehavior: cloudfront.CacheQueryStringBehavior.none()
-        });
+    const allCachePolicy = new cloudfront.CachePolicy(
+      this,
+      'All Cache Policy',
+      {
+        cachePolicyName: 'wpJson',
+        comment: 'Cache policy for the load balancer (efs)',
+        headerBehavior: cloudfront.CacheHeaderBehavior.allowList(
+          'Host',
+          'Referer',
+          'Accept-Language',
+          'User-Agent',
+          'X-Wp-Nonce',
+          'Origin',
+          'Access-Control-Request-Headers',
+          'Access-Control-Request-Method',
+          'Authorization',
+          'CloudFront-Forwarded-Proto'
+        ),
+        cookieBehavior: cloudfront.CacheCookieBehavior.all(),
+        queryStringBehavior: cloudfront.CacheQueryStringBehavior.all(),
+      }
+    );
 
-        const allCachePolicy = new cloudfront.CachePolicy(this, 'All Cache Policy', {
-            cachePolicyName: 'wpJson',
-            comment: 'Cache policy for the load balancer (efs)',
-            headerBehavior: cloudfront.CacheHeaderBehavior.allowList('Host', 'Referer', 'Accept-Language', 'User-Agent', 'X-Wp-Nonce', 'Origin', 'Access-Control-Request-Headers', 'Access-Control-Request-Method', 'Authorization', 'CloudFront-Forwarded-Proto'),
-            cookieBehavior: cloudfront.CacheCookieBehavior.all(),
-            queryStringBehavior: cloudfront.CacheQueryStringBehavior.all()
-        });
+    const defaultBehavior: cloudfront.BehaviorOptions = {
+      origin: new origins.LoadBalancerV2Origin(lb as any, {
+        protocolPolicy: cloudfront.OriginProtocolPolicy.HTTPS_ONLY,
+      }),
+      allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+      viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD_OPTIONS,
+      cachePolicy: defaultCachePolicy,
+    };
 
-        const defaultBehavior: cloudfront.BehaviorOptions = {
-            origin: new origins.LoadBalancerV2Origin(lb as any, {
-                protocolPolicy: cloudfront.OriginProtocolPolicy.HTTPS_ONLY
-            }),
-            allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
-            viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-            cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD_OPTIONS,
-            cachePolicy: defaultCachePolicy
-        }
+    const allBehaviour: cloudfront.BehaviorOptions = {
+      origin: new origins.LoadBalancerV2Origin(lb as any, {
+        protocolPolicy: cloudfront.OriginProtocolPolicy.HTTPS_ONLY,
+      }),
+      allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+      viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD_OPTIONS,
+      cachePolicy: allCachePolicy,
+    };
 
-        const allBehaviour: cloudfront.BehaviorOptions = {
-            origin: new origins.LoadBalancerV2Origin(lb as any, {
-                protocolPolicy: cloudfront.OriginProtocolPolicy.HTTPS_ONLY
-            }),
-            allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
-            viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-            cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD_OPTIONS,
-            cachePolicy: allCachePolicy,
-        }
+    const wpincludes: cloudfront.BehaviorOptions = {
+      origin: new origins.S3Origin(s3 as any),
+      allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+      viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD_OPTIONS,
+      cachePolicy: s3Policy,
+    };
 
-        const wpincludes: cloudfront.BehaviorOptions = {
-            origin: new origins.S3Origin(s3 as any),
-            allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
-            viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-            cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD_OPTIONS,
-            cachePolicy: s3Policy   
-        }
+    const wpContent: cloudfront.BehaviorOptions = {
+      origin: new origins.S3Origin(s3 as any),
+      allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+      viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD_OPTIONS,
+      cachePolicy: s3Policy,
+    };
 
-
-        const wpContent: cloudfront.BehaviorOptions = {
-            origin: new origins.S3Origin(s3 as any),
-            allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
-            viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-            cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD_OPTIONS,
-            cachePolicy: s3Policy   
-        }
-
-        //  Add the load balancer as the origin
-        const cf = new cloudfront.Distribution(this, 'Load balancer distribution', {
-            defaultBehavior: defaultBehavior,
-            domainNames: ['dev.sahamidiamonds.co.uk'],
-            certificate: myCertificate
-        });
-    }
+    //  Add the load balancer as the origin
+    const cf = new cloudfront.Distribution(this, 'Load balancer distribution', {
+      defaultBehavior: defaultBehavior,
+      domainNames: [
+        'dev.sahamidiamonds.co.uk',
+        'www.worldwideandweb.com',
+        'worldwideandweb.com',
+        'www.worldwideandweb.co.uk',
+        'worldwideandweb.co.uk',
+      ],
+      certificate: myCertificate,
+    });
+  }
 }
--- lib/wordpress_infra-stack_instance_loadbalancer.ts
+++ lib/wordpress_infra-stack_instance_loadbalancer.ts
@@ -7,57 +7,85 @@ export class WordpressInfraStackLoadBalancer extends cdk.Stack {
   public asg: AutoScalingGroup;
   public lb: elbv2.ApplicationLoadBalancer;
   public instance: ec2.Instance;
-  constructor(scope: cdk.Construct, id: string, vpc: ec2.Vpc, props?: cdk.StackProps) {
+  constructor(
+    scope: cdk.Construct,
+    id: string,
+    vpc: ec2.Vpc,
+    props?: cdk.StackProps
+  ) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    this.lb = new elbv2.ApplicationLoadBalancer(this, 'Wordpress Application Load Balancer', {
-      vpc,
-      internetFacing: true,
-      vpcSubnets: {
-        availabilityZones: ['eu-west-2a', 'eu-west-2b'],
-        onePerAz: true,
-        subnetType: ec2.SubnetType.PUBLIC
+    this.lb = new elbv2.ApplicationLoadBalancer(
+      this,
+      'Wordpress Application Load Balancer',
+      {
+        vpc,
+        internetFacing: true,
+        vpcSubnets: {
+          availabilityZones: ['eu-west-2a', 'eu-west-2b'],
+          onePerAz: true,
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
       }
-    });
+    );
 
     const listener = this.lb.addListener('Listener 443', {
       port: 443,
-      certificates: [elbv2.ListenerCertificate.fromArn('arn:aws:acm:eu-west-2:460234074473:certificate/038d08bc-8641-4b2b-ad2d-020b9f17d420'),
-    elbv2.ListenerCertificate.fromArn('arn:aws:acm:eu-west-2:460234074473:certificate/c6b0bee3-0103-4b69-824e-322b43d708ff')]
+      certificates: [
+        elbv2.ListenerCertificate.fromArn(
+          'arn:aws:acm:eu-west-2:460234074473:certificate/c6b0bee3-0103-4b69-824e-322b43d708ff'
+        ),
+        elbv2.ListenerCertificate.fromArn(
+          'arn:aws:acm:eu-west-2:460234074473:certificate/e78455fc-e4bc-40b0-8326-b03fab550a74',
+        )
+      ],
     });
 
     this.asg = new AutoScalingGroup(this, 'Wordpress Autoscaling Group', {
       instanceType: new ec2.InstanceType('t2.micro'),
       machineImage: ec2.MachineImage.genericLinux({
-        'eu-west-2': 'ami-01879c3ef34c1ed0a'
+        'eu-west-2': 'ami-0d7b7afb271fabc7e',
       }),
       vpc,
       vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC
+        subnetType: ec2.SubnetType.PUBLIC,
       },
-      keyName: 'Wordpress Load Balancer'
+      keyName: 'Wordpress Load Balancer',
     });
 
-    this.asg.connections.allowFrom(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'SSH from anywhere');
-    this.asg.connections.allowFrom(this.lb, ec2.Port.tcp(80), 'Load Balancer Port');
-    this.asg.connections.allowFrom(ec2.Peer.anyIpv4(), ec2.Port.allTraffic(), 'Load Balancer Port 2');
+    this.asg.connections.allowFrom(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'SSH from anywhere'
+    );
+    this.asg.connections.allowFrom(
+      this.lb,
+      ec2.Port.tcp(80),
+      'Load Balancer Port'
+    );
+    this.asg.connections.allowFrom(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.allTraffic(),
+      'Load Balancer Port 2'
+    );
 
     listener.addTargets('Listener target', {
       port: 80,
-      targets: [this.asg]
+      targets: [this.asg],
     });
 
     this.asg.userData.addCommands(
-      "apt-get -y update",
-      "apt-get -y upgrade",                                 // Ubuntu: apt-get -y upgrade
-      "apt-get -y install amazon-efs-utils",                // Ubuntu: apt-get -y install amazon-efs-utils
-      "apt-get -y install nfs-common",                       // Ubuntu: apt-get -y install nfs-common
-      "file_system_id_1=fs-a9d5fa58",
-      "efs_mount_point_1=/mnt/efs/fs1",
-      "mkdir -p \"${efs_mount_point_1}\"",
-      "test -f \"/sbin/mount.efs\" && echo \"${file_system_id_1}:/ ${efs_mount_point_1} efs defaults,_netdev\" >> /etc/fstab || " +
-      "echo \"${file_system_id_1}.efs.eu-west-2.amazonaws.com:/ ${efs_mount_point_1} nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0\" >> /etc/fstab",
-      "mount -a -t efs,nfs4 defaults");
+      'apt-get -y update',
+      'apt-get -y upgrade', // Ubuntu: apt-get -y upgrade
+      'apt-get -y install amazon-efs-utils', // Ubuntu: apt-get -y install amazon-efs-utils
+      'apt-get -y install nfs-common', // Ubuntu: apt-get -y install nfs-common
+      'file_system_id_1=fs-a9d5fa58',
+      'efs_mount_point_1=/mnt/efs/fs1',
+      'mkdir -p "${efs_mount_point_1}"',
+      'test -f "/sbin/mount.efs" && echo "${file_system_id_1}:/ ${efs_mount_point_1} efs defaults,_netdev" >> /etc/fstab || ' +
+        'echo "${file_system_id_1}.efs.eu-west-2.amazonaws.com:/ ${efs_mount_point_1} nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0" >> /etc/fstab',
+      'mount -a -t efs,nfs4 defaults'
+    );
   }
 }
